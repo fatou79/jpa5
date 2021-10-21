8c8461991717accde76eebe6cf661985d73bfd6f
coumba <diagne@gmail.com>
2021-10-21 15:24:06 +0000

    Jpa5

---
+++ src/main/java/com/fatou/demoJpa5/DemoJpa5Application.java
@@ -0,0 +1,13 @@
+package com.fatou.demoJpa5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpa5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpa5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatou/demoJpa5/DemoJpa5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatou.demoJpa5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpa5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
